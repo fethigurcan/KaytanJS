dc049aaa3cf4233c7d91bdeaab1d94540d235bf0
Fethi Gürcan <fethigurcan@gmail.com>
2020-01-08 03:55:58 +0300

    It works! :)

--- kaytancmd.js
+++ kaytancmd.js
@@ -1,8 +1,9 @@
 Kaytan=require('.');
 
-dd=new Kaytan("{{<tmp}} deneme {{/}}merhaba {{@dee}} {{$ggg}}{{#deneme}} {{>tmp}}ddd {{:}} {{aaa}}eee{{&bbb}} {{/}}");
+dd=new Kaytan("{{<tmp}} deneme {{.}} {{/}}merhaba {{?!a|b|c}}logic var{{:}}logic var ama değişken yok{{/}} {{@dee}} {{$ggg}}{{?$ggg}}global ggg{{/}}{{^$ooo}}global ooo{{/}}{{#deneme}} {{^#first}} AND {{/}}{{>tmp}}ddd {{:}} {{aaa}}eee{{&bbb}} {{/}}");
 console.log(dd);
 //dd.execute({ deneme: 'fethi' });
 
 console.log(dd.ast.toString());
+console.log(dd.execute({ deneme:[1,2,3] }));
 console.log('end');
