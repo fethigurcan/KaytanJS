c0a1980cd05c4181f60d728712041216eced94f9
Mobula Mobular <mobulaco@users.noreply.github.com>
2020-01-13 18:23:19 +0300

    support new test case format

--- test-server/index.js
+++ test-server/index.js
@@ -2,8 +2,8 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require("fs");
-var testlist,testlisthtml;
-const testsPath=path.resolve("test-data");
+const testlist=require("../test-data");
+const _escape=v=>v.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;").replace(/\//g, "&#x2F;");
 
 let port = 4444
 const index = Math.max(process.argv.indexOf('--port'), process.argv.indexOf('-p'))
@@ -11,10 +11,12 @@ if (index !== -1) {
   port = +process.argv[index + 1] || port
 }
 
+let list=testlist();
+
 const app = express()
   .use(bodyParser.urlencoded({ extended: true }))
   .use("/dist",express.static(path.resolve('dist')))
-  .post("/test-case",(req, res) => {
+  .post("/test-data",(req, res) => {
       if (!fs.existsSync(testsPath)){
         fs.mkdirSync(testsPath);
       }
@@ -38,42 +40,26 @@ const app = express()
         </body>
       </html>`);
   })
+  .get("/test-data/:block/:test",(req, res) => {
+    list=list||testlist();
+    res.contentType="application/json";
+    res.send(fs.readFileSync(list[req.params.block][req.params.test]));
+  })
   .get("/:jslibrary",(req, res) => {
     
-    if (!testlist){
-      const _escape=v=>v.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;").replace(/'/g, "&#039;").replace(/\//g, "&#x2F;");
-      testlist = JSON.parse(fs.readFileSync(path.resolve("test/testlist.json")));
-      testlisthtml='<select id="list" onchange="select()" autofocus><option></option>';
-
-      for (let block in testlist){
-        let blockItem=testlist[block];
-        let lasttemplate,lastobj,lastexpectedResult;
-        testlisthtml+=`<optgroup label="${_escape(block)}">`;
-        for (let title in blockItem){
-          let _title=_escape(title);
-          let template,obj,expectedResult;
-          let item=blockItem[title];
-          
-          template=item[0]===-1?lasttemplate:item[0];
-          obj=item[1]===-1?lastobj:item[1];
-          expectedResult=item[2]===-1?lastexpectedResult:item[2];
-          
-          //this approach prevents value changes in parallel processing
-          lasttemplate=template;
-          lastobj=obj;
-          lastexpectedResult=expectedResult;
-
-          //prepare simple data for to pass client side.
-          item[0]=template;
-          item[1]=obj;
-          item[2]=expectedResult;
-
-          testlisthtml+=`<option value="${_title}">${_title}</option>`;
-        }
-        testlisthtml+="</optgroup>";
+    list=testlist(); //update every page load or TODO: listen fs
+    let testlisthtml='<select id="list" onchange="select()" autofocus><option></option>';
+    for (let block in list){
+      let blockItem=list[block];
+      testlisthtml+=`<optgroup label="${_escape(block)}">`;
+      for (let title in blockItem){
+        let _title=_escape(title);
+        let item=blockItem[title];
+        testlisthtml+=`<option value="${_title}">${_title}</option>`;
       }
-      testlisthtml+=`</select><script>var testlist=${JSON.stringify(testlist)};</script>`;
+      testlisthtml+="</optgroup>";
     }
+    testlisthtml+=`</select>`;
     
     res.send(
       `<!DOCTYPE html>
@@ -82,7 +68,7 @@ const app = express()
           <title>Test ${req.params.jslibrary}.js</title>
           <style>
             table { width:100%; }
-            textarea { width:100%; resize:none;font-family:monospace; }
+            textarea { width:100%; resize:vertical;font-family:monospace; }
             pre:empty:before { content: '(Empty)'; }
             #expectedlabel { color:red; }
           </style>
@@ -99,15 +85,15 @@ const app = express()
               let outputhiddenel=document.getElementById('output-hidden');
               let templateel=document.getElementById('template');
               let datael=document.getElementById('data');
+              let expectedhiddenel=document.getElementById('expectedhidden');
+
               try{
                 let data=JSON.parse(datael.value);
                 let kaytan=new Kaytan(template.value);
                 let output=kaytan.execute(data);
                 outputhiddenel.value=output;
                 outputel.innerText=_escape(output);
-                if (selected){
-                  document.getElementById('expectedlabel').style.color=output==selected[2]?'green':null;
-                }
+                document.getElementById('expectedlabel').style.color=output==expectedhiddenel.value?'green':null;
               }catch(e){
                 debugger;
                 outputhiddenel.value=JSON.stringify({ error:e.name,message:e.message });
@@ -120,31 +106,43 @@ const app = express()
               let outputel=document.getElementById('output');
               let outputhiddenel=document.getElementById('output-hidden');
               let expectedel=document.getElementById('expected');
+              let expectedhiddenel=document.getElementById('expectedhidden');
               let templateel=document.getElementById('template');
               let testnameel=document.getElementById('testname');
               let groupnameel=document.getElementById('groupname');
+              let submitel=document.getElementById('submit');
               let datael=document.getElementById('data');
               
               if (event.target.value){
-                selectedGroup=testlist[event.target.selectedOptions[0].parentElement.label];
-                selected=selectedGroup[event.target.value];
-                testnameel.value=event.target.value;
-                groupnameel.value=event.target.selectedOptions[0].parentElement.label;
+                selectedGroup=event.target.selectedOptions[0].parentElement.label;
+                selected=event.target.value;
+                testnameel.value=selected;
+                groupnameel.value=selectedGroup;
 
-                outputel.innerText="";
-                outputhiddenel.value="";
-                templateel.value=selected[0]?selected[0]:"";
-                datael.value=JSON.stringify(selected[1]);
-                expectedel.innerText=selected[2];
-                expectedel.parentElement.parentElement.parentElement.style.display=null;
-                onExecute();
+                let xhr=new XMLHttpRequest();
+                xhr.open("GET",encodeURI("/test-data/"+selectedGroup+"/"+selected+""));
+                xhr.onload = function() {
+                  outputel.innerText="";
+                  outputhiddenel.value="";
+                  let test=JSON.parse(xhr.responseText);
+                  templateel.value=test.template;
+                  datael.value=JSON.stringify(test.data);
+                  expectedel.innerText=test.expected;
+                  expectedhiddenel.value=test.expected;
+                  expectedel.parentElement.parentElement.style.display=null;
+                  onExecute();
+                  submitel.disabled=null;
+                };
+                submitel.disabled="disabled";
+                xhr.send();
               }else{
                 testnameel.value=event.target.value;
                 groupnameel.value="";
                 selectedGroup=null;
                 selected=null;
                 expectedel.innerText="";
-                expectedel.parentElement.parentElement.parentElement.style.display="none";
+                expectedhiddenel.value="";
+                expectedel.parentElement.parentElement.style.display="none";
                 document.getElementById('expectedlabel').style.color=null;
               }
             }
@@ -156,8 +154,10 @@ const app = express()
               document.getElementById('output-hidden').value="";
 
               let expectedel=document.getElementById('expected');
+              let expectedhiddenel=document.getElementById('expectedhidden');
               expectedel.innerText="";
-              expectedel.parentElement.parentElement.parentElement.style.display="none";
+              expectedhiddenel.value="";
+              expectedel.parentElement.parentElement.style.display="none";
             }
 
             function onSubmit(){
@@ -165,35 +165,34 @@ const app = express()
               return true;
             }
           </script>
-          <form id="form" action="/test-case" method="post" onsubmit="return onSubmit()">
+          <form id="form" action="/test-data" method="post" onsubmit="return onSubmit()">
             <table>
-                <tr><td>
+                <tr colspan="2"><td>
                 Test:
                 ${testlisthtml}
                 <input type="text" id="groupname" name="groupname" required/>
                 <input type="text" id="testname" name="testname" required/>
+                <button id="execute" name="execute" type="button" accesskey="e" onclick="onExecute()">Execute</button> <input type="submit" id="submit" accesskey="s" value="Save As Test Case">
               </td></tr>
-              <tr><td>
+              <tr><td width="50%">
                 Template:
                 <div><textarea id="template" name="template" rows="10" onchange="userchange()">Hello {{who}}!</textarea></div>
-              </td></tr>
-              <tr><td>
+              </td>
+              <td width="50%">
                 JSON Data:
-                <div><textarea id="data" name="data" rows="5" onchange="userchange()">{ "who":"World" }</textarea></div>
+                <div><textarea id="data" name="data" rows="10" onchange="userchange()">{ "who":"World" }</textarea></div>
               </td></tr>
-              <tr><td>
+              <tr><td valign="top">
                 Output:
                 <div><pre id="output"></pre><input type="hidden" id="output-hidden" name="output" required/></div>
-              </td></tr>
-              <tr style="display:none"><td>
+              </td>
+              <td style="display:none" valign="top">
                 <span id="expectedlabel">Expected:</span>
                 <div><pre id="expected"></pre></div>
               </td></tr>
-              <tr><td style="text-align:center">
-                <button id="execute" name="execute" type="button" accesskey="e" onclick="onExecute()">Execute</button> <input type="submit" accesskey="s" value="Save As Test Case">
-              </td></tr>
               </table>
             </form>
+            <input type="hidden" id="expectedhidden"/>
         </body>
       </html>`
     );
